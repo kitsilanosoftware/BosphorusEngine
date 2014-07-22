6d8b828a9363cf2044ca769be76d4a9b6af450ec
Damien Diederen <dd@crosstwine.com>
2014-07-22 17:29:54 +0200

    UnityEngine: Initial RenderSettings & Color compatibility types

--- Disunity.UnityEngine/UnityEngine.cs
+++ Disunity.UnityEngine/UnityEngine.cs
@@ -1,5 +1,101 @@
 ﻿using System;
+using YamlDotNet.Serialization;
 
 namespace Disunity.UnityEngine
 {
+	public struct Color
+	{
+		public Color (float r, float g, float b, float a)
+		{
+			this.r = r;
+			this.g = g;
+			this.b = b;
+			this.a = a;
+		}
+
+		public Color (float r, float g, float b)
+		{
+			this.r = r;
+			this.g = g;
+			this.b = b;
+			this.a = 1.0f;
+		}
+
+		// public static Color Lerp (Color a, Color b, float t);
+		// public override bool Equals (object other);
+		// public override int GetHashCode ();
+
+		public override string ToString ()
+		{
+			return String.Format ("RGBA({0}, {1}, {2}, {3})", r, g, b, a);
+		}
+
+		// public string ToString (string format);
+
+		// public static Color operator + (Color a, Color b);
+		// public static Color operator - (Color a, Color b);
+		// public static Color operator * (Color a, Color b);
+		// public static Color operator * (Color a, float b);
+		// public static Color operator * (float b, Color a);
+		// public static Color operator / (Color a, float b);
+		// public static bool operator == (Color lhs, Color rhs);
+		// public static bool operator != (Color lhs, Color rhs);
+		// public static implicit operator Vector4 (Color c);
+		// public static implicit operator Color (Vector4 v);
+
+		// public static Color black { get; }
+		// public static Color blue { get; }
+		// public static Color clear { get; }
+		// public static Color cyan { get; }
+		// public static Color gray { get; }
+		// public static Color green { get; }
+		// public static Color grey { get; }
+		// public static Color magenta { get; }
+		// public static Color red { get; }
+		// public static Color white { get; }
+		// public static Color yellow { get; }
+		// public Color gamma { get; }
+		// public float grayscale { get; }
+		// public float this [int index] { get; set; }
+		// public Color linear { get; }
+
+		public float r;
+		public float g;
+		public float b;
+		public float a;
+	}
+
+	public class RenderSettings
+	{
+		[YamlAlias("m_AmbientLight")]
+		public /* static */ Color ambientLight { get; set; }
+
+		[YamlAlias("m_FlareFadeSpeed")]
+		public /* static */ float flareFadeSpeed { get; set; }
+
+		[YamlAlias("m_FlareStrength")]
+		public /* static */ float flareStrength { get; set; }
+
+		[YamlAlias("m_Fog")]
+		public /* static */ bool fog { get; set; }
+
+		[YamlAlias("m_FogColor")]
+		public /* static */ Color fogColor { get; set; }
+
+		[YamlAlias("m_FogDensity")]
+		public /* static */ float fogDensity { get; set; }
+
+		[YamlAlias("m_LinearFogEnd")]
+		public /* static */ float fogEndDistance { get; set; }
+
+		// public static FogMode fogMode { get; set; }
+
+		[YamlAlias("m_LinearFogStart")]
+		public /* static */ float fogStartDistance { get; set; }
+
+		[YamlAlias("m_HaloStrength")]
+		public /* static */ float haloStrength { get; set; }
+
+		// public static Material skybox { get; set; }
+	}
 }
