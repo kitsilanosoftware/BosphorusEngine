4790967d93acf36f31e0e491574a766e23ebc1d7
Damien Diederen <dd@crosstwine.com>
2014-07-24 19:12:07 +0200

    UnityEngine: Add skeleton for MonoBehaviour

--- Disunity.UnityEngine/UnityEngine.cs
+++ Disunity.UnityEngine/UnityEngine.cs
@@ -1104,4 +1104,26 @@ namespace Disunity.UnityEngine
 		[YamlAlias("m_WrapMode")]
 		public WrapMode wrapMode { get; set; }
 	}
+
+	public class MonoBehaviour: Behaviour
+	{
+		// public MonoBehaviour ();
+
+		// public static void print (object message);
+		// public void CancelInvoke ();
+		// public void CancelInvoke (string methodName);
+		// public void Invoke (string methodName, float time);
+		// public void InvokeRepeating (string methodName, float time, float repeatRate);
+		// public bool IsInvoking ();
+		// public bool IsInvoking (string methodName);
+		// public Coroutine StartCoroutine (System.Collections.IEnumerator routine);
+		// public Coroutine StartCoroutine (string methodName);
+		// public Coroutine StartCoroutine (string methodName, object value);
+		// public Coroutine StartCoroutine_Auto (System.Collections.IEnumerator routine);
+		// public void StopAllCoroutines ();
+		// public void StopCoroutine (System.Collections.IEnumerator routine);
+		// public void StopCoroutine (string methodName);
+
+		// public bool useGUILayout { get; set; }
+	}
 }
