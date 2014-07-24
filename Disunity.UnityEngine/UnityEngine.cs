09ed91be1ed6e4416d1c2a21568d4149e08404c7
Damien Diederen <dd@crosstwine.com>
2014-07-24 19:25:25 +0200

    UnityEngine: Add skeleton for Camera, with dependencies

--- Disunity.UnityEngine/UnityEngine.cs
+++ Disunity.UnityEngine/UnityEngine.cs
@@ -1134,4 +1134,193 @@ namespace Disunity.UnityEngine
 		[YamlAlias("m_SourcePositions")]
 		public Vector3[] probePositions { get; set; }
 	}
+
+	[Serializable]
+	public enum CameraClearFlags
+	{
+		Skybox = 1,
+		Color = 2,
+		SolidColor = 2,
+		Depth = 3,
+		Nothing = 4
+	}
+
+	public struct Rect
+	{
+		// public Rect (float left, float top, float width, float height);
+		// public Rect (Rect source);
+
+		// public static Rect MinMaxRect (float left, float top, float right, float bottom);
+		// public static Vector2 NormalizedToPoint (Rect rectangle, Vector2 normalizedRectCoordinates);
+		// public static Vector2 PointToNormalized (Rect rectangle, Vector2 point);
+		// public bool Contains (Vector2 point);
+		// public bool Contains (Vector3 point);
+		// public bool Contains (Vector3 point, bool allowInverse);
+		// public override bool Equals (object other);
+		// public override int GetHashCode ();
+		// public bool Overlaps (Rect other);
+		// public bool Overlaps (Rect other, bool allowInverse);
+		// public void Set (float left, float top, float width, float height);
+		// public override string ToString ();
+		// public string ToString (string format);
+
+		// public static bool operator != (Rect lhs, Rect rhs);
+		// public static bool operator == (Rect lhs, Rect rhs);
+
+		// public float bottom { get; }
+		// public Vector2 center { get; set; }
+
+		// YAML: Direct mapping.
+		public float height { get; set; }
+
+		// public float left { get; }
+		// public Vector2 max { get; set; }
+		// public Vector2 min { get; set; }
+		// public Vector2 position { get; set; }
+		// public float right { get; }
+		// public Vector2 size { get; set; }
+		// public float top { get; }
+
+		// YAML: Direct mapping.
+		public float width { get; set; }
+		public float x { get; set; }
+
+		// public float xMax { get; set; }
+		// public float xMin { get; set; }
+
+		// YAML: Direct mapping.
+		public float y { get; set; }
+
+		// public float yMax { get; set; }
+		// public float yMin { get; set; }
+	}
+
+	[Serializable]
+	public enum RenderingPath {
+		UsePlayerSettings = -1,
+		VertexLit = 0,
+		Forward,
+		DeferredLighting
+	}
+
+	public sealed class Camera: Behaviour
+	{
+		// public Camera ();
+
+		// public static int GetAllCameras (Camera[] cameras);
+		// public static void SetupCurrent (Camera cur);
+		// public Matrix4x4 CalculateObliqueMatrix (Vector4 clipPlane);
+		// public void CopyFrom (Camera other);
+		// public void DoClear ();
+		// public float GetScreenHeight ();
+		// public float GetScreenWidth ();
+		// public void Render ();
+		// public void RenderDontRestore ();
+		// public bool RenderToCubemap (Cubemap cubemap);
+		// public bool RenderToCubemap (Cubemap cubemap, int faceMask);
+		// public bool RenderToCubemap (RenderTexture cubemap);
+		// public bool RenderToCubemap (RenderTexture cubemap, int faceMask);
+		// public void RenderWithShader (Shader shader, string replacementTag);
+		// public void ResetAspect ();
+		// public void ResetProjectionMatrix ();
+		// public void ResetReplacementShader ();
+		// public void ResetWorldToCameraMatrix ();
+		// public Ray ScreenPointToRay (Vector3 position);
+		// public Vector3 ScreenToViewportPoint (Vector3 position);
+		// public Vector3 ScreenToWorldPoint (Vector3 position);
+		// public void SetReplacementShader (Shader shader, string replacementTag);
+		// public void SetTargetBuffers (RenderBuffer colorBuffer, RenderBuffer depthBuffer);
+		// public void SetTargetBuffers (RenderBuffer[] colorBuffer, RenderBuffer depthBuffer);
+		// public Ray ViewportPointToRay (Vector3 position);
+		// public Vector3 ViewportToScreenPoint (Vector3 position);
+		// public Vector3 ViewportToWorldPoint (Vector3 position);
+		// public Vector3 WorldToScreenPoint (Vector3 position);
+		// public Vector3 WorldToViewportPoint (Vector3 position);
+
+		// public static Camera[] allCameras { get; }
+		// public static int allCamerasCount { get; }
+		// public static Camera current { get; }
+		// public static Camera main { get; }
+		// public static Camera mainCamera { get; }
+		// public RenderingPath actualRenderingPath { get; }
+		// public float aspect { get; set; }
+
+		[YamlAlias("m_BackGroundColor")]
+		public Color backgroundColor { get; set; }
+
+		// public Matrix4x4 cameraToWorldMatrix { get; }
+
+		[YamlAlias("m_ClearFlags")]
+		public CameraClearFlags clearFlags { get; set; }
+
+		// public bool clearStencilAfterLightingPass { get; set; }
+
+		// TODO: Indirect mapping.
+		// [YamlAlias("m_CullingMask")]
+		// public int cullingMask { get; set; }
+
+		[YamlAlias("m_Depth")]
+		public float depth { get; set; }
+
+		// public DepthTextureMode depthTextureMode { get; set; }
+		// public int eventMask { get; set; }
+		// public float far { get; set; }
+
+		[YamlAlias("far clip plane")]
+		public float farClipPlane { get; set; }
+
+		[YamlAlias("field of view")]
+		public float fieldOfView { get; set; }
+
+		// public float fov { get; set; }
+
+		[YamlAlias("m_HDR")]
+		public bool hdr { get; set; }
+
+		// public bool isOrthoGraphic { get; set; }
+		// public float [] layerCullDistances { get; set; }
+		// public bool layerCullSpherical { get; set; }
+		// public float near { get; set; }
+
+		[YamlAlias("near clip plane")]
+		public float nearClipPlane { get; set; }
+
+		// YAML: Direct mapping.
+		public bool orthographic { get; set; }
+
+		[YamlAlias("orthographic size")]
+		public float orthographicSize { get; set; }
+
+		// public float pixelHeight { get; }
+		// public Rect pixelRect { get; set; }
+		// public float pixelWidth { get; }
+		// public Matrix4x4 projectionMatrix { get; set; }
+
+		// TODO: Verify.
+		[YamlAlias("m_NormalizedViewPortRect")]
+		public Rect rect { get; set; }
+
+		[YamlAlias("m_RenderingPath")]
+		public RenderingPath renderingPath { get; set; }
+
+		[YamlAlias("m_StereoConvergence")]
+		public float stereoConvergence { get; set; }
+
+		// public bool stereoEnabled { get; }
+
+		[YamlAlias("m_StereoSeparation")]
+		public float stereoSeparation { get; set; }
+
+		// TODO: type, fileID mapping.
+		// [YamlAlias("m_TargetTexture")]
+		// public RenderTexture targetTexture { get; set; }
+
+		// public TransparencySortMode transparencySortMode { get; set; }
+
+		[YamlAlias("m_OcclusionCulling")]
+		public bool useOcclusionCulling { get; set; }
+
+		// public Vector3 velocity { get; }
+		// public Matrix4x4 worldToCameraMatrix { get; set; }
+	}
 }
