65645fa7dc158b8c4bf5214879d0c0ab4284f361
Bob Summerwill <bob@summerwill.net>
2014-09-29 15:21:57 -0700

    Added SceneSettings.cs and switching to Xamarin Studio.

---
+++ Source/SceneSettings.cs
@@ -0,0 +1,25 @@
+//------------------------------------------------------------------------------
+// SceneSettings.cs
+//
+// This file is part of the Bosphorus project.
+//
+// See http://bosphorusengine.com for more details on Bosphorus.
+//
+// Copyright (c) 2014 Kitsilano Software Inc (http://kitsilanosoftware.com)
+//------------------------------------------------------------------------------
+
+using YamlDotNet.Serialization;
+
+namespace UnityEngine
+{
+	public class SceneSettings : Object
+	{
+		//m_PVSData: 
+		//m_PVSObjectsArray: []
+		//m_PVSPortalsArray: []
+		//m_OcclusionBakeSettings:
+			//smallestOccluder: 1
+			//smallestHole: .25
+			//backfaceThreshold: 100
+	}
+}
